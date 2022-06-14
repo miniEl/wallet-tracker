005e48ccddda8c81977d1122ceb5a3e4cad0313c
Merhan Elkheshen <merhan.elkheshen@ibm.com>
2022-06-14 21:31:00 +0200

    FIX: App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import useLocalStorage from "use-local-storage";
 import Header from "./components/Header/Header";
-import ExpenseList from "./components/ExpenseList/ExpenseList";
+import ExpenseList from "./components/Expenses/ExpenseList/ExpenseList";
 import Footer from "./components/Footer";
 import './App.scss';
 
@@ -11,22 +11,27 @@ const App = () => {
     const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
 
     const toggleTheme = () => {
-      setTheme(theme === 'light' ? 'dark' : 'light');
+        setTheme(theme === 'light' ? 'dark' : 'light');
     }
 
-    useEffect(() => {      
-      document.body.setAttribute('data-theme', theme);
-      return () => {
-        document.body.removeAttribute('data-theme');
-      };
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme);
+        return () => {
+            document.body.removeAttribute('data-theme');
+        };
     }, [theme])
 
-    return ( 
-      <div className="app">
-        <Header theme={theme} onToggleTheme={toggleTheme} />
-        <ExpenseList />
-        <Footer />
-      </div>
+    return ( <
+        div className = "app" >
+        <
+        Header theme = { theme }
+        onToggleTheme = { toggleTheme }
+        /> <
+        ExpenseList / >
+        <
+        Footer / >
+        <
+        /div>
     );
 }
 
