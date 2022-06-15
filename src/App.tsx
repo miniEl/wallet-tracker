95e1c503f26b868a78b802c08df86ba20de393c8
Merhan Elkheshen <merhan.elkheshen@ibm.com>
2022-06-15 03:40:25 +0200

    NO COMMENT

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
+import useTheme from "./Custom-Hooks/useTheme";
 import Header from "./components/Header/Header";
 import ExpenseList from "./components/Expenses/ExpenseList/ExpenseList";
 import Footer from "./components/Footer";
 import './App.scss';
-import useTheme from "./Custom-Hooks/useTheme";
 
 
 const App = () => {
