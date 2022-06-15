c2b55c78096c435bad630a40d3e59b1bf851f294
Merhan Elkheshen <merhan.elkheshen@ibm.com>
2022-06-15 03:08:30 +0200

    FIX: Card component

--- src/components/UI/Card/Card.tsx
+++ src/components/UI/Card/Card.tsx
@@ -1,6 +1,6 @@
 import './Card.scss';
 
-const Card = (props) => {
+const Card = (props: any) => {
   const classes = 'card ' + props.className;
   
   return <div className={classes}>{props.children}</div>
