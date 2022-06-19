f6789328bfb4c7a4b2dfa3f1f6490479051bb3f4
Merhan Elkheshen <merhan.elkheshen@ibm.com>
2022-06-19 03:04:25 +0200

    Created: useCurrencyFormatter hook

--- src/components/Expenses/ExpenseItem/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem/ExpenseItem.tsx
@@ -1,13 +1,16 @@
+import useCurrencyFormatter from '../../../Helpers/useCurrencyFormatter';
 import './ExpenseItem.scss';
 
 const ExpenseItem = (props: any) => {
+
+  const amount = useCurrencyFormatter('usd', props.amount);
 
   return (
     <div className='expense-item'>
       <div className='expense-item__details'>
         <h4>{props.title}</h4>
         <div className='align-right'>
-        <div className='expense-item__amount'>£{props.amount}</div>
+        <div className='expense-item__amount'>{amount}</div>
         <div className='expense-item__date secondary-txt'>{props.date}</div>
         </div>
       </div>
