41b221ee70bff78b8cb500592dacb6321fec0488
Merhan Elkheshen <merhan.elkheshen@ibm.com>
2022-06-23 06:24:05 +0200

    UPDATE Hooks

---
+++ src/Hooks/useCurrencyFormatter.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+const useCurrencyFormatter = () => {
+  const [amount, setAmount] = useState('');
+
+  const setFormat:any = (currency?: any, value?: any) => {
+    const currencyFormatter = new Intl.NumberFormat(undefined, {
+      currency: currency,
+      style: "currency",
+      minimumFractionDigits: 0,
+    });
+
+    setAmount(currencyFormatter.format(value));
+  }
+  
+  return [amount, setFormat];
+}
+
+export default useCurrencyFormatter;
